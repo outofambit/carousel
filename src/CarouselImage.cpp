52eff46dfca960d5acef16cb0ea701cd00e40337
Evelyn Masso <evoutofambit@gmail.com>
2014-03-17 23:30:51 -0700

    store photo year according to dir name

--- src/CarouselImage.cpp
+++ src/CarouselImage.cpp
@@ -13,6 +13,8 @@ using namespace ci;
 CarouselImage::CarouselImage(const fs::path p)
 {
     mBasePath = p;
+    mYear = std::atoi( p.filename().string().c_str() );
+    app::console() << mYear << std::endl;
 }
 
 void CarouselImage::setup()
