23d4dc3087166af67d7de531d7219433f20021e7
Evelyn Masso <evoutofambit@gmail.com>
2014-05-01 18:48:10 -0700

    BigImageLoader files

---
+++ include/BigImageLoader.h
@@ -0,0 +1,14 @@
+//
+//  BigImageLoader.h
+//  carousel
+//
+//  Created by Nicholas Masso on 5/1/14.
+//
+//
+
+#ifndef __carousel__BigImageLoader__
+#define __carousel__BigImageLoader__
+
+#include <iostream>
+
+#endif /* defined(__carousel__BigImageLoader__) */
---
+++ src/BigImageLoader.cpp
@@ -0,0 +1,9 @@
+//
+//  BigImageLoader.cpp
+//  carousel
+//
+//  Created by Nicholas Masso on 5/1/14.
+//
+//
+
+#include "BigImageLoader.h"
